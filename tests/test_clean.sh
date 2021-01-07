a3ebb56f96093f0e569781590c7453762036b547
Brelliothe <yrx18@mails.tsinghua.edu.cn>
2021-01-07 17:31:24 +0800

    Update test_clean.sh

--- tests/test_clean.sh
+++ tests/test_clean.sh
@@ -1,14 +1,14 @@
 gcc -Wall -D_FILE_OFFSET_BITS=64 ../main.c ../metadata.c `pkg-config fuse3 --cflags --libs` -o lfs
-gcc test_lfs.c -o test_lfs
+gcc test_clean.c -o test_clean
 mkdir mount
 ./lfs mount
 mkdir test
 touch a.txt
 sleep 0.1
-./test_lsf
+./test_clean
 rm -rf test
 rm a.txt
 umount ./lfs
 rm -rf mount
 rm lfs
-rm test_lfs
+rm test_clean
